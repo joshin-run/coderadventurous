7853d0f961d5f223c366d0fad7d1d5ae728af891
joshin-run <jahmsc@gmail.com>
2017-08-22 12:56:36 -0600

    Added Sofi exersize for table/text color loop

---
+++ scripts/sofi/sofi.js
@@ -0,0 +1,38 @@
+// Pseudo code
+//
+// get the content of each cell
+
+function letterArr() {
+  var elArr = 
+  var letter = document.getElementsByTagName("td")[i].textContent;
+}
+
+
+
+// let letter = function() {
+//   var elem = document.getElementsByTagName('td');
+//   var innerText = elem[0].innerHTML;
+//   return innerText;
+// }
+// console.log(letter);
+
+
+
+
+
+// function myfunction(foo) {
+//     var elem = foo.getElementsByTagName('p');
+//     var TextInsideLi = elem[0].innerHTML;
+// }​
+
+// loop through each '<td>'
+// goal to add each letter to an array and return/log
+// let letterArr = [];
+// if '<td>' cell is the same color as it's text, then it is not added
+
+
+// identify the color of each letter in each cell
+// identify the background color of each cell
+// if the letter is a different color from the background color of the cell it is displayed in, then add that letter to the array.
+// if the letter is the same color as the background it is displayed in, do not add that letter to the array
+// return the array
