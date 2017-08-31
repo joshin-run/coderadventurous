1ef7904a16c929832adc9b817ab9787bfca1b1f4
joshin-run <jahmsc@gmail.com>
2017-08-31 16:17:18 -0600

    Add button and function

--- js-world/js/main.js
+++ js-world/js/main.js
@@ -1,3 +1,18 @@
 //let first = document.GetElementById('')
 
 // Statement: each individual instruction or step
+
+let button = document.getElementById('changeArray');
+button.addEventListener('click', () => {
+  document.getElementById('show').innerHTML = "let array = [1, 2, 3]; array[2] = 1; let el = document.getElementById('show'); el.textContent = array[2];";
+});
+
+/* objects */
+var foo = 
+
+// function changeArray(() => alert('josh'));
+// setTimeout(() => console.log(this.sound), 1000);
+// const fullNames5 = names.map(() => `Cool Bos`);
+//
+// const sayMyName = (name) => {alert(`Hello ${name}!`)}
+// sayMyName('Wes');
